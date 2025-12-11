80bf4a8d141cc8f58196e11b76f0250639e597c0
dayniel-caadiang <dayniel.caadiang@betterask.erni>
2025-12-11 15:13:32 +0800

    1

--- build.sh
+++ build.sh
@@ -1,10 +1,7 @@
 #!/usr/bin/env bash
-# exit on error
 set -o errexit
 
 pip install -r requirements.txt
-
 python manage.py collectstatic --no-input
 python manage.py migrate
-
-python manage.py create_superuser
+python manage.py createadmin
---
+++ orders/management/commands/__init__.py
@@ -0,0 +1,17 @@
+from django.core.management.base import BaseCommand
+from django.contrib.auth import get_user_model
+
+class Command(BaseCommand):
+    help = 'Create superuser automatically'
+
+    def handle(self, *args, **kwargs):
+        User = get_user_model()
+        if not User.objects.filter(username='admin').exists():
+            User.objects.create_superuser(
+                username='admin',
+                email='admin@logistics.com',
+                password='Admin@12345'
+            )
+            self.stdout.write(self.style.SUCCESS('Superuser created successfully!'))
+        else:
+            self.stdout.write(self.style.WARNING('Superuser already exists'))
