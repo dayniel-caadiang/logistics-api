437df7aae531bad062a98a148b8ffb7d5988c424
dayniel-caadiang <dayniel.caadiang@betterask.erni>
2025-12-11 13:46:58 +0800

    1

--- build.sh
+++ build.sh
@@ -1,14 +1,8 @@
 #!/usr/bin/env bash
+# exit on error
 set -o errexit
 
-# Install dependencies
 pip install -r requirements.txt
 
-# Set Python path to current directory
-export PYTHONPATH="${PYTHONPATH}:$(pwd)"
-
-# Collect static files
 python manage.py collectstatic --no-input
-
-# Run database migrations
 python manage.py migrate
